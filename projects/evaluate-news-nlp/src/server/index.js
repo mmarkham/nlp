036872d7c7f4e3c90ec107c8b3f50ce10ecd5431
DESKTOP-AI8FUNF\Matt <mattmarkham1@gmail.com>
2022-01-04 16:21:31 -0800

    add jest

--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -1,23 +1,32 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+var path = require('path');
+const express = require('express');
+const mockAPIResponse = require('./mockAPI.js');
 
-const app = express()
+const dotenv = require('dotenv');
+dotenv.config({path: '../../.env'});
 
-app.use(express.static('dist'))
+console.log(`Your API key is ${process.env.API_KEY}`);
+const baseURL = "https://api.meaningcloud.com/sentiment-2.1?key=";
+const apiKey = process.env.API_KEY;
 
-console.log(__dirname)
+const app = express();
+
+app.use(express.static('dist'));
+
+console.log(__dirname);
 
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile(path.resolve('src/client/views/index.html'));
 })
 
 // designates what port the app will listen to for incoming requests
 app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+    console.log('Example app listening on port 8080!');
 })
 
 app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+    res.send(mockAPIResponse);
+});
+
+
